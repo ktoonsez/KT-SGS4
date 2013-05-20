ab4444527cba5949245aa9d9673ba8e67c2746b8
ktoonsez <ktoonsez@gmail.com>
2013-05-20 16:42:25 -0700

    Remove a WHOLE ASS LOAD of dmesg spamming

--- drivers/net/usb/rmnet_usb_ctrl.c
+++ drivers/net/usb/rmnet_usb_ctrl.c
@@ -192,7 +192,7 @@ static void notification_available_cb(struct urb *urb)
 	case 0:
 	/*if non zero lenght of data received while unlink*/
 	case -ENOENT:
-		pr_debug("[NACB:%d]<", iface_num);
+		//pr_info("[NACB:%d]<", iface_num);
 		/*success*/
 		break;
 
@@ -667,8 +667,8 @@ ctrl_read:
 	DBG("%s: Returning %d bytes to %s\n", __func__, bytes_to_read,
 			dev->name);
 
-	snprintf(temp, sizeof(temp), "[%lluns]READ :", rd_poll_delta_time);
-	DUMP_BUFFER(temp, bytes_to_read, buf);
+	//snprintf(temp, sizeof(temp), "[%lluns]READ :", rd_poll_delta_time);
+	//DUMP_BUFFER(temp, bytes_to_read, buf);
 
 	return bytes_to_read;
 }
@@ -703,7 +703,7 @@ static ssize_t rmnet_ctl_write(struct file *file, const char __user * buf,
 		kfree(wbuf);
 		return status;
 	}
-	DUMP_BUFFER("Write: ", size, buf);
+	//DUMP_BUFFER("Write: ", size, buf);
 
 	status = rmnet_usb_ctrl_write(dev, wbuf, size);
 	if (status == size)
