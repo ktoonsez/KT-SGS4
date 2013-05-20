1cec5a4a5f625b79a8e15ed1b73f52c478595a71
ktoonsez <ktoonsez@gmail.com>
2013-05-20 10:34:19 -0700

    Update build scripts for TMO AND SPR

--- build_tmo_tw.sh
+++ build_tmo_tw.sh
@@ -2,7 +2,7 @@
 export KERNELDIR=`readlink -f .`
 export PARENT_DIR=`readlink -f ..`
 export INITRAMFS_DEST=$KERNELDIR/kernel/usr/initramfs
-export INITRAMFS_SOURCE=`readlink -f ..`/Ramdisks/TW
+export INITRAMFS_SOURCE=`readlink -f ..`/Ramdisks/TW_TMO
 export CONFIG_AOSP_BUILD=y
 export PACKAGEDIR=$PARENT_DIR/Packages/TW
 #Enable FIPS mode
