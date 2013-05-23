0066e84ebd0fe1cbbeea59c06f5e665a58fcc290
ktoonsez <ktoonsez@gmail.com>
2013-05-23 07:12:33 -0700

    cpufreq: Fix dbs_attr_group for SLP

--- drivers/cpufreq/cpufreq_slp.c
+++ drivers/cpufreq/cpufreq_slp.c
@@ -820,7 +820,7 @@ static struct attribute *dbs_attributes[] = {
 
 static struct attribute_group dbs_attr_group = {
 	.attrs = dbs_attributes,
-	.name = "pegasusq",
+	.name = "slp",
 };
 
 /************************** sysfs end ************************/
