91aa6269828281e0565d25b9ee37d940e9f354b6
ktoonsez <ktoonsez@gmail.com>
2013-05-24 12:40:51 -0700

    Add build all branches script

---
+++ build_all_branches.sh
@@ -0,0 +1,48 @@
+echo "Switch to AOSP 4.2.2"
+git checkout aosp
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att_aosp.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_spr_aosp.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_tmo_aosp.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_vzw_aosp.sh
+
+echo "Switch to Touchjizz 4.2.2"
+git checkout tw
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att_tw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_spr_tw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_tmo_tw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_vzw_tw.sh
+
