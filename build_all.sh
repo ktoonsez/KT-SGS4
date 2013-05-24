9ffe77e3a52da99e8d804a58f109c33c50cdb08d
ktoonsez <ktoonsez@gmail.com>
2013-05-24 12:39:02 -0700

    Add build all variants script

---
+++ build_all.sh
@@ -0,0 +1,20 @@
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att_tw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_spr_tw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_tmo_tw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_vzw_tw.sh
