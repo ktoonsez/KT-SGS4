e9cb394fa531ae94a970f43e6e2445eb104ea85e
ktoonsez <ktoonsez@gmail.com>
2013-05-24 17:40:44 -0700

    Linux 3.4.9

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -324,6 +324,7 @@ void machine_shutdown(void)
 void machine_halt(void)
 {
 	machine_shutdown();
+	local_irq_disable();
 	while (1);
 }
 
@@ -349,6 +350,7 @@ void machine_restart(char *cmd)
 
 	/* Whoops - the platform was unable to reboot. Tell the user! */
 	printk("Reboot failed -- System halted\n");
+	local_irq_disable();
 	while (1);
 }
 
