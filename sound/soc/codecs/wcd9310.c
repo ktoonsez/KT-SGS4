7d775ccee2bb0b420f232aec1b2b4bdb5be3321b
ktoonsez <ktoonsez@gmail.com>
2013-05-26 15:07:20 -0700

    Add Faux sound

--- sound/soc/codecs/wcd9310.c
+++ sound/soc/codecs/wcd9310.c
@@ -8827,6 +8827,11 @@ static const struct file_operations poke_reg_fops = {
 };
 #endif
 
+#ifdef CONFIG_SOUND_CONTROL_HAX_GPL
+struct snd_kcontrol_new *gpl_faux_snd_controls_ptr =
+		(struct snd_kcontrol_new *)tabla_snd_controls;
+#endif
+
 static int tabla_codec_probe(struct snd_soc_codec *codec)
 {
 	struct wcd9xxx *control;
