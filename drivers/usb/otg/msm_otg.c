6842f25a076f1937b995068a6a605666efa1577f
ktoonsez <ktoonsez@gmail.com>
2013-06-09 11:05:15 -0700

    Fast Charge: Fix up and add some crazy ass amount of code to disable PC USB MTP recognition and make sure 1000 mA is applied

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -219,7 +219,7 @@ static int msm_hsusb_config_vddcx(int high)
 		return ret;
 	}
 
-	pr_info("%s: min_vol:%d max_vol:%d\n", __func__, min_vol, max_vol);
+	pr_info("KTFAST_CHARGE-%s: min_vol:%d max_vol:%d\n", __func__, min_vol, max_vol);
 
 	return ret;
 }
@@ -1189,17 +1189,18 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 			"Failed notifying %d charger type to PMIC\n",
 							motg->chg_type);
 
-	if (motg->cur_power == mA)
-		return;
 #ifdef CONFIG_FORCE_FAST_CHARGE
-  if (force_fast_charge == 1) {
-      mA = USB_FASTCHG_LOAD;
-      pr_info("USB fast charging is ON - 1000mA.\n");
-  } else {
-    pr_info("USB fast charging is OFF.\n");
-  }
+  	if (force_fast_charge == 1) {
+      		pr_info("USB fast charging is ON - 1000mA, %d.\n", mA);
+      		mA = USB_FASTCHG_LOAD;
+  	} else {
+    		pr_info("USB fast charging is OFF, %d.\n", mA);
+  	}
 #endif
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
+
+	if (motg->cur_power == mA)
+		return;
 
 	pm8921_charger_vbus_draw(mA);
 	msm_otg_notify_power_supply(motg, mA);
@@ -1567,7 +1568,10 @@ static void msm_otg_start_peripheral(struct usb_otg *otg, int on)
 
 	if (!otg->gadget)
 		return;
-
+	
+	if (force_fast_charge == 1 && on == 1)
+		on = 0;
+		
 #ifdef CONFIG_USB_HOST_NOTIFY
 	if (on == 1)
 		motg->ndev.mode = NOTIFY_PERIPHERAL_MODE;
@@ -2512,6 +2516,7 @@ static void msm_otg_sm_work(struct work_struct *w)
 				case USB_CDP_CHARGER:
 					msm_otg_notify_charger(motg,
 							IDEV_CHG_MAX);
+					pr_alert("KTOTG-USB_CDP_CHARGER");
 					msm_otg_start_peripheral(otg, 1);
 					otg->phy->state =
 						OTG_STATE_B_PERIPHERAL;
@@ -2519,6 +2524,7 @@ static void msm_otg_sm_work(struct work_struct *w)
 				case USB_ACA_C_CHARGER:
 					msm_otg_notify_charger(motg,
 							IDEV_ACA_CHG_MAX);
+					pr_alert("KTOTG-USB_ACA_C_CHARGER");
 					msm_otg_start_peripheral(otg, 1);
 					otg->phy->state =
 						OTG_STATE_B_PERIPHERAL;
@@ -2527,9 +2533,15 @@ static void msm_otg_sm_work(struct work_struct *w)
 					msm_otg_notify_charger(motg,
 							IDEV_CHG_MIN);
 					if(!slimport_is_connected()) {
+						pr_alert("KTOTG-USB_SDP_CHARGER");
 						msm_otg_start_peripheral(otg, 1);
-						otg->phy->state =
-							OTG_STATE_B_PERIPHERAL;
+						//if (force_fast_charge == 0)
+							otg->phy->state = OTG_STATE_B_PERIPHERAL;
+						//else
+						//{
+						//	otg->phy->state = OTG_STATE_UNDEFINED;
+						//	otg->gadget->is_a_peripheral = 0;
+						//}
 					}
 					schedule_delayed_work(&motg->check_ta_work,
 						MSM_CHECK_TA_DELAY);
@@ -2938,6 +2950,7 @@ static void msm_otg_sm_work(struct work_struct *w)
 			otg->phy->state = OTG_STATE_A_PERIPHERAL;
 			msm_otg_host_hnp_enable(otg, 1);
 			otg->gadget->is_a_peripheral = 1;
+			pr_alert("KTOTG-OTG_STATE_A_SUSPEND");
 			msm_otg_start_peripheral(otg, 1);
 		} else if (!test_bit(B_CONN, &motg->inputs) &&
 				!otg->host->b_hnp_enable) {
