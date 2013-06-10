88751bdf9fd4faf7d960a5bb6008c5849e401f0c
ktoonsez <ktoonsez@gmail.com>
2013-06-09 21:15:13 -0700

    Revert "jf: update to MDL source drop"
    This reverts commit e37d73c7a016f218dfbd8c54d5f930bb48f3a225.

--- arch/arm/kernel/sys_arm.c
+++ arch/arm/kernel/sys_arm.c
@@ -212,6 +212,7 @@ asmlinkage int sys_execve(const char __user *filenamei,
 							"PPID = %d(%s)\n",
 				current->pid, current->comm,
 				current->parent->pid, current->parent->comm);
+			panic("ROOTING_PREVENTION");
 			return -EACCES;
 		}
 #endif	// End of CONFIG_SEC_RESTRICT_FORK
