1974b45e33348754bd6f4e39e5f1fd2d7a0741ed
Matt Filetto <matt.filetto@gmail.com>
2013-06-17 22:31:47 -0700

    GPU: available freqs: adjust for 544 to 545mhz change
    Change-Id: I4c0ada48361e295009484de8c801db707ed2c5d7

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -685,7 +685,7 @@ static int kgsl_pwrctrl_gpu_available_frequencies_show(
 		{
 			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",627000000);
 			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",600000000);
-			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",544000000);
+			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",545000000);
 			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",504000000);
 			num_chars += snprintf(buf + num_chars, PAGE_SIZE, "%d ",450000000);
 		}
