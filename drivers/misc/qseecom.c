690a897c874660f1beaf6feed9087f1904b59dca
ktoonsez <ktoonsez@gmail.com>
2013-07-21 08:51:05 -0700

    Bye moronic QSEECOM: qseecom_ioctl: failed qseecom_receive_req: -512

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -2041,8 +2041,8 @@ static long qseecom_ioctl(struct file *file, unsigned cmd,
 		ret = qseecom_receive_req(data);
 		atomic_dec(&data->ioctl_count);
 		wake_up_all(&data->abort_wq);
-		if (ret)
-			pr_err("failed qseecom_receive_req: %d\n", ret);
+		//if (ret)
+		//	pr_err("failed qseecom_receive_req: %d\n", ret);
 		break;
 	}
 	case QSEECOM_IOCTL_SEND_RESP_REQ: {
