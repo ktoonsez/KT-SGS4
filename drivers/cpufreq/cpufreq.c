f4fc9506bab7829cc9dccdf82b1d956479af93ee
ktoonsez <ktoonsez@gmail.com>
2013-07-24 19:06:40 -0700

    cpufreq: Remove SetExtraCores and created a new function called set_cpu_min_max which will set the cores specified using a workqueue to free up the calling tasks like screen on/off, bluetooth and charging mhz functions.  This should help with the slow screen wake issue that some people experience

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -97,6 +97,12 @@ extern void apenable_auto_hotplug(bool state);
 
 static bool never_set[10];
 
+struct work_struct set_cpu_min_max_work;
+static unsigned int work_speed_min;
+static unsigned int work_speed_max;
+static unsigned int work_speed_core_start;
+static struct workqueue_struct *dbs_wq;
+
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
  * level driver of CPUFreq support, and its spinlock. This lock
@@ -506,87 +512,78 @@ static ssize_t store_##file_name					\
 	return ret ? ret : count;					\
 }
 
-void __ref SetExtraCores(unsigned int min, unsigned int max)
+static void __cpuinit set_cpu_min_max_work_fn(struct work_struct *work)
 {
-	struct cpufreq_policy new_policy;
-	int cpu, ret;
-	for (cpu = 1; cpu < CPUS_AVAILABLE; cpu++)
+	if (work_speed_min > 0 || work_speed_max > 0)
 	{
-		if (!cpu_online(cpu)) cpu_up(cpu);
-		//if (&trmlpolicy[cpu] != NULL)
-		usleep(50);
-		if (cpu_online(cpu))
+		struct cpufreq_policy new_policy;
+		int cpu, ret;
+		for (cpu = work_speed_core_start; cpu < CPUS_AVAILABLE; cpu++)
 		{
-			struct cpufreq_policy *policyorig = cpufreq_cpu_get_sysfs(cpu);
-			if (policyorig)
+			if (!cpu_online(cpu)) cpu_up(cpu);
+			usleep(50);
+			if (cpu_online(cpu))
 			{
-				ret = cpufreq_get_policy(&new_policy, cpu);
-				if (min)
-					new_policy.min = min;
-				if (max)
-					new_policy.max = max;
-				pr_alert("SET EXTRA CORES 1 - %d - %d - %d - %d - %d - %d - %d", cpu, policyorig->cpu, new_policy.min, new_policy.max, policyorig->min, policyorig->max, policyorig->user_policy.max);
-				__cpufreq_set_policy(policyorig, &new_policy);
-				if (min)
-					policyorig->user_policy.min = policyorig->min;
-				if (max)
-					policyorig->user_policy.max = policyorig->max;
-				pr_alert("SET EXTRA CORES 2 - %d - %d - %d - %d - %d - %d - %d", cpu, policyorig->cpu, new_policy.min, new_policy.max, policyorig->min, policyorig->max, policyorig->user_policy.max);
-			}
-		}				
+				struct cpufreq_policy *policyorig = cpufreq_cpu_get_sysfs(cpu);
+				if (policyorig)
+				{
+					ret = cpufreq_get_policy(&new_policy, cpu);
+					if (work_speed_min)
+						new_policy.min = work_speed_min;
+					if (work_speed_max)
+						new_policy.max = work_speed_max;
+					pr_alert("SET EXTRA CORES 1 - %d - %d - %d - %d - %d - %d - %d", cpu, policyorig->cpu, new_policy.min, new_policy.max, policyorig->min, policyorig->max, policyorig->user_policy.max);
+					__cpufreq_set_policy(policyorig, &new_policy);
+					if (work_speed_min)
+						policyorig->user_policy.min = policyorig->min;
+					if (work_speed_max)
+						policyorig->user_policy.max = policyorig->max;
+					pr_alert("SET EXTRA CORES 2 - %d - %d - %d - %d - %d - %d - %d", cpu, policyorig->cpu, new_policy.min, new_policy.max, policyorig->min, policyorig->max, policyorig->user_policy.max);
+				}
+			}				
+		}
 	}
 }
 
-void __ref send_cable_state(unsigned int state)
+static void set_cpu_min_max(unsigned int min, unsigned int max, unsigned int core_start)
+{
+	work_speed_min = min;
+	work_speed_max = max;
+	work_speed_core_start = core_start;
+	queue_work_on(0, dbs_wq, &set_cpu_min_max_work);
+}
+
+void send_cable_state(unsigned int state)
 {
 	unsigned int min = 0;
 	unsigned int max = 0;
 	if (state)
 	{
 		if (Lcharging_min_mhz && ((bluetooth_scaling_mhz_active && Lcharging_min_mhz > Lbluetooth_scaling_mhz) || !bluetooth_scaling_mhz_active))
-		{
-			struct cpufreq_policy new_policy;
-			int cpu, ret;
-			cpufreq_set_limit_defered(USER_MIN_START, Lcharging_min_mhz);
 			min = Lcharging_min_mhz;
-			Lcharging_mhz_active = true;
-		}
 		if (Lcharging_max_mhz)
-		{
-			struct cpufreq_policy new_policy;
-			int cpu, ret;
-			cpufreq_set_limit_defered(USER_MAX_START, Lcharging_max_mhz);
 			max = Lcharging_max_mhz;
-			Lcharging_mhz_active = true;
-		}
 		pr_alert("CHARGING MHZ ON %u-%u-%u-%u\n", Lcharging_min_mhz, Lcharging_max_mhz, min, max);
 		if (min > 0 || max > 0)
-			SetExtraCores(min, max);
+		{
+			Lcharging_mhz_active = true;
+			set_cpu_min_max(min, max, 0);
+		}
 	}
 	else if (Lcharging_mhz_active)
 	{
 		unsigned int value;
 		value = Lcharging_min_mhz_orig;
 		if (value && !Lcharging_mhz_active_block_min && ((bluetooth_scaling_mhz_active && value > Lbluetooth_scaling_mhz) || !bluetooth_scaling_mhz_active))
-		{
-			struct cpufreq_policy new_policy;
-			int cpu, ret;
-			cpufreq_set_limit_defered(USER_MIN_START, value);
 			min = value;
-		}
 		value = Lcharging_max_mhz_orig;
 		if (value && !Lcharging_mhz_active_block_max)
-		{
-			struct cpufreq_policy new_policy;
-			int cpu, ret;
-			cpufreq_set_limit_defered(USER_MAX_START, value);
 			max = value;
-		}
 		if (!Lcharging_mhz_active_block_min && !Lcharging_mhz_active_block_max)
 			Lcharging_mhz_active = false;
 		pr_alert("CHARGING MHZ OFF %u-%u-%u-%u\n", Lcharging_min_mhz, Lcharging_max_mhz, min, max);
 		if (min > 0 || max > 0)
-			SetExtraCores(min, max);
+			set_cpu_min_max(min, max, 0);
 	}
 }
 
@@ -616,7 +613,7 @@ static ssize_t __ref store_scaling_min_freq(struct cpufreq_policy *policy, const
 
 	//Set extra CPU cores to same speed
 	if (policy->cpu == 0)
-		SetExtraCores(value, 0);
+		set_cpu_min_max(value, 0, 1);
 	
 	Lbluetooth_scaling_mhz_orig = value;
 	Lcharging_min_mhz_orig = value;
@@ -651,7 +648,7 @@ static ssize_t __ref store_scaling_max_freq(struct cpufreq_policy *policy, const
 		
 		//Set extra CPU cores to same speed
 		if (policy->cpu == 0)
-			SetExtraCores(0, value);
+			set_cpu_min_max(0, value, 1);
 		
 		Lscreen_off_scaling_mhz_orig = value;
 		Lcharging_max_mhz_orig = value;
@@ -1303,8 +1300,7 @@ unsigned int set_battery_max_level(unsigned int value)
 		}
 		if (!Lcharging_mhz_active)
 		{
-			cpufreq_set_limit_defered(USER_MAX_START, value);
-			SetExtraCores(0, value);
+			set_cpu_min_max(0, value, 0);
 		}
 		pr_alert("SET_BATTERY_MAX_LEVEL: %u\n", value);
 	}
@@ -1328,8 +1324,7 @@ void set_bluetooth_state(unsigned int val)
 		{
 			bluetooth_scaling_mhz_active = true;
 			value = Lbluetooth_scaling_mhz;
-			cpufreq_set_limit_defered(USER_MIN_START, value);
-			SetExtraCores(value, 0);
+			set_cpu_min_max(value, 0, 0);
 		}
 		else
 		{
@@ -1337,8 +1332,7 @@ void set_bluetooth_state(unsigned int val)
 			value = Lbluetooth_scaling_mhz_orig;
 			if ((Lcharging_mhz_active && value > Lcharging_min_mhz) || !Lcharging_mhz_active)
 			{
-				cpufreq_set_limit_defered(USER_MIN_START, value);
-				SetExtraCores(value, 0);
+				set_cpu_min_max(value, 0, 0);
 			}
 		}
 	}
@@ -2730,8 +2724,7 @@ static void cpufreq_gov_resume(void)
 			value = mhz_lvl;
 		if (!Lcharging_mhz_active)
 		{
-			cpufreq_set_limit_defered(USER_MAX_START, value);
-			SetExtraCores(0, value);
+			set_cpu_min_max(0, value, 0);
 		}
 		pr_alert("cpufreq_gov_resume_freq: %u\n", value);
 	}
@@ -2789,8 +2782,7 @@ static void cpufreq_gov_suspend(void)
 				value = mhz_lvl;
 			if (!Lcharging_mhz_active)
 			{
-				cpufreq_set_limit_defered(USER_MAX_START, value);
-				SetExtraCores(0, value);
+				set_cpu_min_max(0, value, 0);
 			}
 			pr_alert("cpufreq_gov_suspend_freq: %u\n", value);
 		}
@@ -3036,6 +3028,9 @@ int cpufreq_unregister_driver(struct cpufreq_driver *driver)
 	cpufreq_driver = NULL;
 	spin_unlock_irqrestore(&cpufreq_driver_lock, flags);
 
+	cancel_work_sync(&set_cpu_min_max_work);
+	destroy_workqueue(dbs_wq);
+	
 	return 0;
 }
 EXPORT_SYMBOL_GPL(cpufreq_unregister_driver);
@@ -3056,6 +3051,13 @@ static int __init cpufreq_core_init(void)
 	BUG_ON(!cpufreq_global_kobject);
 	register_syscore_ops(&cpufreq_syscore_ops);
 
+	dbs_wq = alloc_workqueue("cpufreq_coreminmax_wq", WQ_HIGHPRI, 0);
+	if (!dbs_wq) {
+		printk(KERN_ERR "Failed to create cpufreq_coreminmax_wq workqueue\n");
+		return -EFAULT;
+	}
+	INIT_WORK(&set_cpu_min_max_work, set_cpu_min_max_work_fn);
+	
 	return 0;
 }
 core_initcall(cpufreq_core_init);
