250aa076a694026695df56c58ef49ef0fac11556
ktoonsez <ktoonsez@gmail.com>
2013-08-04 09:34:12 -0700

    build_all_branches: Update for both 4.3 branches

--- build_all_branches.sh
+++ build_all_branches.sh
@@ -56,6 +56,64 @@ echo "Make Mrproper"
 make mrproper
 ./build_intl.sh
 
+echo "Switch to AOSP 4.3"
+git checkout aosp4.3
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_spr.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_tmo.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_vzw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_intl.sh
+
+echo "Switch to Touchjizz 4.3"
+git checkout tw4.3
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_spr.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_tmo.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_vzw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_intl.sh
+
 echo "Switch to AOSP 4.2.2"
 git checkout aosp
 
