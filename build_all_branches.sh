ec973f176daddebe384efaaa8a2aa07297ebde03
ktoonsez <ktoonsez@gmail.com>
2013-11-14 06:49:48 -0700

    Update build all branches for the new branches added

--- build_all_branches.sh
+++ build_all_branches.sh
@@ -1,5 +1,34 @@
-echo "Switch to AOSP 4.2.2"
-git checkout aosp
+echo "Switch to AOSP 4.4"
+git checkout aosp4.4
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_spr.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_tmo.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_vzw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_intl.sh
+
+echo "Switch to AOSP 4.3"
+git checkout aosp4.3
 
 echo "Make Clean"
 make clean
@@ -56,6 +85,64 @@ echo "Make Mrproper"
 make mrproper
 ./build_intl.sh
 
-echo "Switch to AOSP 4.2.2"
-git checkout aosp
+echo "Switch to Touchjizz 4.3"
+git checkout tw4.3
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_spr.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_tmo.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_vzw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_intl.sh
+
+echo "Switch to Touchjizz GE 4.3"
+git checkout twge4.3
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_spr.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_tmo.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_vzw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_intl.sh
+
+echo "Switch to AOSP 4.4"
+git checkout aosp4.4
 
