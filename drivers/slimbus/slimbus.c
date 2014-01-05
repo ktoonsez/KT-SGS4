bfaf9d2a217224870fbd2bb9d9a153ee95dd8d07
ktoonsez <ktoonsez@gmail.com>
2014-01-04 19:28:50 -0700

    ktoonservativeq: Add disable hotplugging setting while playing media

--- drivers/slimbus/slimbus.c
+++ drivers/slimbus/slimbus.c
@@ -47,6 +47,8 @@ static struct delayed_work music_is_playing;
 static bool is_music_playing = false;
 static bool is_music_playing_func_sent = false;
 extern bool set_music_playing_state(bool val);
+extern void set_music_playing_statekt(bool state);
+static bool ktoonservative_is_active = false;
 
 static const struct slim_device_id *slim_match(const struct slim_device_id *id,
 					const struct slim_device *slim_dev)
@@ -157,11 +159,20 @@ struct device slimbus_dev = {
 	.init_name = "slimbus",
 };
 
+void ktoonservative_is_active_media(bool val)
+{
+	ktoonservative_is_active = val;
+}
+
 static void music_is_playing_fn(struct work_struct *work)
 {
 	//pr_alert("MUSIC PLAYING (FUNC) - KTOONSEZ - %d", is_music_playing);
 	if (is_music_playing)
+	{
 		is_music_playing_func_sent = set_music_playing_state(is_music_playing);
+		if (ktoonservative_is_active)
+			set_music_playing_statekt(is_music_playing);
+	}
 }
 
 static void __exit slimbus_exit(void)
@@ -2914,7 +2925,11 @@ int slim_control_ch(struct slim_device *sb, u16 chanh,
 		{
 			is_music_playing = false;
 			if (is_music_playing_func_sent)
+			{
 				set_music_playing_state(is_music_playing);
+				if (ktoonservative_is_active)
+					set_music_playing_statekt(is_music_playing);
+			}
 			is_music_playing_func_sent = false;
 			//pr_alert("MUSIC STOPPED - KTOONSEZ");
 		}
