27e05e03fe48a4ce9104e0eb696c83730406dc98
ktoonsez <ktoonsez@gmail.com>
2014-02-05 10:46:38 -0700

    GPU: Some simple rewrite, make laziness and ramp threshold tunable and add default_lvl
    Conflicts:
    	drivers/gpu/msm/kgsl_pwrscale_trustzone.c

--- drivers/gpu/msm/kgsl_pwrscale_trustzone.c
+++ drivers/gpu/msm/kgsl_pwrscale_trustzone.c
@@ -45,11 +45,14 @@ spinlock_t tz_lock;
  * per frame for 60fps content.
  */
 #define FLOOR			5000
+#define CEILING			50000
 #define SWITCH_OFF		200
 #define SWITCH_OFF_RESET_TH	40
 #define SKIP_COUNTER		500
 #define TZ_RESET_ID		0x3
 #define TZ_UPDATE_ID		0x4
+
+static int default_lvl = 1;
 
 #ifdef CONFIG_MSM_SCM
 /* Trap into the TrustZone, and call funcs there. */
@@ -112,48 +115,102 @@ static ssize_t tz_governor_store(struct kgsl_device *device,
 	else if (!strncmp(str, "performance", 11))
 		priv->governor = TZ_GOVERNOR_PERFORMANCE;
 
-	if (priv->governor == TZ_GOVERNOR_PERFORMANCE)
+	if (priv->governor == TZ_GOVERNOR_PERFORMANCE) {
 		kgsl_pwrctrl_pwrlevel_change(device, pwr->max_pwrlevel);
+		pwr->default_pwrlevel = pwr->max_pwrlevel;
+	} else {
+		pwr->default_pwrlevel = default_lvl;
+	}
 
 	mutex_unlock(&device->mutex);
 	return count;
 }
 
-PWRSCALE_POLICY_ATTR(governor, 0644, tz_governor_show, tz_governor_store);
-
-static struct attribute *tz_attrs[] = {
-	&policy_attr_governor.attr,
-	NULL
-};
-
-static struct attribute_group tz_attr_group = {
-	.attrs = tz_attrs,
-};
-
-static void tz_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
+static ssize_t tz_default_lvl_show(struct kgsl_device *device,
+				struct kgsl_pwrscale *pwrscale,
+				char *buf)
 {
-	struct tz_priv *priv = pwrscale->priv;
-	if (device->state != KGSL_STATE_NAP &&
-#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
-		(priv->governor == TZ_GOVERNOR_ONDEMAND ||
-		 priv->governor == TZ_GOVERNOR_SIMPLE))
-#else
-		priv->governor == TZ_GOVERNOR_ONDEMAND)
-#endif
-		kgsl_pwrctrl_pwrlevel_change(device,
-					device->pwrctrl.default_pwrlevel);
+	return sprintf(buf, "%u\n", default_lvl);
+}
+
+static ssize_t tz_default_lvl_store(struct kgsl_device *device,
+				struct kgsl_pwrscale *pwrscale,
+				 const char *buf, size_t count)
+{
+
+	unsigned int input;
+	int ret;
+	struct kgsl_pwrctrl *pwr = &device->pwrctrl;
+
+	ret = sscanf(buf, "%u", &input);
+	if (ret < 0 || input > 3)
+		return -EINVAL;
+
+	default_lvl = input;
+	pwr->default_pwrlevel = default_lvl;
+	return count;
 }
 
 #ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
 /* KGSL Simple GPU Governor */
 /* Copyright (c) 2011-2013, Paul Reioux (Faux123). All rights reserved. */
 static int default_laziness = 5;
-module_param_named(simple_laziness, default_laziness, int, 0664);
+static int laziness = 0;
+//module_param_named(simple_laziness, default_laziness, int, 0664);
 
 static int ramp_up_threshold = 6000;
-module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
+//module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
 
-static int laziness;
+static ssize_t tz_simple_laziness_show(struct kgsl_device *device,
+				struct kgsl_pwrscale *pwrscale,
+				char *buf)
+{
+	return sprintf(buf, "%u\n", default_laziness);
+}
+
+static ssize_t tz_simple_laziness_store(struct kgsl_device *device,
+				struct kgsl_pwrscale *pwrscale,
+				 const char *buf, size_t count)
+{
+
+	unsigned int input;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+
+	if (ret < 0 || input > 1000)
+		return -EINVAL;
+
+	default_laziness = input;
+	return count;
+}
+
+static ssize_t tz_simple_ramp_threshold_show(struct kgsl_device *device,
+				struct kgsl_pwrscale *pwrscale,
+				char *buf)
+{
+	return sprintf(buf, "%u\n", ramp_up_threshold);
+}
+
+static ssize_t tz_simple_ramp_threshold_store(struct kgsl_device *device,
+				struct kgsl_pwrscale *pwrscale,
+				 const char *buf, size_t count)
+{
+
+	unsigned int input;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+
+	if (ret < 0 || input > 100000)
+		return -EINVAL;
+
+	ramp_up_threshold = input;
+	return count;
+}
+PWRSCALE_POLICY_ATTR(simple_laziness, 0664, tz_simple_laziness_show, tz_simple_laziness_store);
+PWRSCALE_POLICY_ATTR(simple_ramp_threshold, 0664, tz_simple_ramp_threshold_show, tz_simple_ramp_threshold_store);
+
+//static int logit = 0;
+//static int logit2 = 0;
 
 static int simple_governor(struct kgsl_device *device, int idle_stat)
 {
@@ -183,9 +240,30 @@ static int simple_governor(struct kgsl_device *device, int idle_stat)
 		else if (pwr->active_pwrlevel == (pwr->num_pwrlevels - 1))
 			val = 0; /* already @ min, so do nothing */
 	}
+	/*logit++;
+	if (logit > 10 || val != 0)
+	{
+		pr_alert("GPU SPEED CHECK2: %d %d %d %d\n", idle_stat, laziness, val, pwr->active_pwrlevel);
+		logit = 0;
+	}*/
 	return val;
 }
 #endif
+
+PWRSCALE_POLICY_ATTR(governor, 0644, tz_governor_show, tz_governor_store);
+PWRSCALE_POLICY_ATTR(default_lvl, 0664, tz_default_lvl_show, tz_default_lvl_store);
+
+static struct attribute *tz_attrs[] = {
+	&policy_attr_governor.attr,
+	&policy_attr_default_lvl.attr,
+	&policy_attr_simple_laziness.attr,
+	&policy_attr_simple_ramp_threshold.attr,
+	NULL
+};
+
+static struct attribute_group tz_attr_group = {
+	.attrs = tz_attrs,
+};
 
 static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 {
@@ -227,10 +305,21 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 		priv->no_switch_cnt = 0;
 	}
 
-	idle = priv->bin.total_time - priv->bin.busy_time;
-	priv->bin.total_time = 0;
-	priv->bin.busy_time = 0;
-	idle = (idle > 0) ? idle : 0;
+	/*logit2++;
+	if (logit2 > 10)
+	{
+		pr_alert("GPU SPEED CHECK: %lld %lld\n", priv->bin.busy_time, priv->bin.total_time);
+		logit2 = 0;
+	}*/
+	
+	/* If there is an extended block of busy processing,
+	 * increase frequency.  Otherwise run the normal algorithm.
+	 */
+	if (priv->bin.busy_time > CEILING) {
+		val = -1;
+	} else {
+		idle = priv->bin.total_time - priv->bin.busy_time;
+		idle = (idle > 0) ? idle : 0;
 #ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
 	if (priv->governor == TZ_GOVERNOR_SIMPLE)
 		val = simple_governor(device, idle);
@@ -239,6 +328,9 @@ static void tz_idle(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
 #else
 	val = __secure_tz_entry(TZ_UPDATE_ID, idle, device->id);
 #endif
+	}
+	priv->bin.total_time = 0;
+	priv->bin.busy_time = 0;
 	if (val) {
 		kgsl_pwrctrl_pwrlevel_change(device,
 					     pwr->active_pwrlevel + val);
@@ -262,6 +354,20 @@ static void tz_sleep(struct kgsl_device *device,
 	priv->no_switch_cnt = 0;
 	priv->bin.total_time = 0;
 	priv->bin.busy_time = 0;
+}
+
+static void tz_wake(struct kgsl_device *device, struct kgsl_pwrscale *pwrscale)
+{
+	struct tz_priv *priv = pwrscale->priv;
+	if (device->state != KGSL_STATE_NAP &&
+#ifdef CONFIG_MSM_KGSL_SIMPLE_GOV
+		(priv->governor == TZ_GOVERNOR_ONDEMAND ||
+		 priv->governor == TZ_GOVERNOR_SIMPLE))
+#else
+		priv->governor == TZ_GOVERNOR_ONDEMAND)
+#endif
+		kgsl_pwrctrl_pwrlevel_change(device,
+					device->pwrctrl.default_pwrlevel);
 }
 
 #ifdef CONFIG_MSM_SCM
