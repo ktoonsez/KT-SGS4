bbe6ec26d012e5abed9b1001789b2b9b299a4187
ktoonsez <ktoonsez@gmail.com>
2014-02-24 07:44:22 -0700

    Update build all branches for building thread headers for ease of updating Ops

--- build_all_branches.sh
+++ build_all_branches.sh
@@ -1,5 +1,7 @@
-echo "Switch to AOSP 4.2.2"
-git checkout aosp
+echo "Copy SERVER LINKS FILE"
+cp ../Packages/url/SERVERLINKS_ORIG.txt ../Packages/url/SERVERLINKS.txt
+echo "Switch to AOSP 4.4"
+git checkout aosp4.4
 
 echo "Make Clean"
 make clean
@@ -27,8 +29,8 @@ echo "Make Mrproper"
 make mrproper
 ./build_intl.sh
 
-echo "Switch to Touchjizz 4.2.2"
-git checkout tw
+echo "Switch to AOSP 4.3"
+git checkout aosp4.3
 
 echo "Make Clean"
 make clean
@@ -56,6 +58,110 @@ echo "Make Mrproper"
 make mrproper
 ./build_intl.sh
 
-echo "Switch to AOSP 4.2.2"
-git checkout aosp
+echo "Switch to Touchjizz 4.4"
+git checkout tw4.4
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_spr.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_intl.sh
+
+
+echo "Switch to Touchjizz 4.3"
+git checkout tw4.3
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_spr.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_tmo.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_vzw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_intl.sh
+
+echo "Switch to Touchjizz GE 4.4"
+git checkout twge4.4
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_intl.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_vzw.sh
+
+echo "Switch to Touchjizz GE 4.3"
+git checkout twge4.3
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_att.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_spr.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_tmo.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_vzw.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_intl.sh
+
+echo "Switch to Touchjizz 4.2-jactive"
+git checkout tw-active
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_intlja.sh
+
+echo "Switch to AOSP 4.4"
+git checkout aosp4.4
 
