6800577245c306dbbc954d11ce1e7c6f25cbda48
ktoonsez <ktoonsez@gmail.com>
2014-03-05 20:24:30 -0700

    Add s2w, d2w, w2w and p2w
    Conflicts:
    	drivers/input/touchscreen/synaptics_i2c_rmi.c

--- drivers/sensorhub/factory/prox_max88920.c
+++ drivers/sensorhub/factory/prox_max88920.c
@@ -13,6 +13,8 @@
  *
  */
 #include "../ssp.h"
+#include <linux/wakelock.h>
+#include <linux/pm_wakeup.h>
 
 #define	VENDOR		"MAXIM"
 #define	CHIP_ID		"MAX88920"
@@ -45,6 +47,43 @@
 /*************************************************************************/
 /* factory Sysfs                                                         */
 /*************************************************************************/
+
+/**
+ * struct pmic8xxx_pwrkey - pmic8xxx pwrkey information
+ * @key_press_irq: key press irq number
+ * @pdata: platform data
+ */
+struct pmic8xxx_pwrkey {
+	struct input_dev *pwr;
+	int key_press_irq;
+	u32	powerkey_state ;
+	int key_release_irq;
+	bool press;
+	const struct pm8xxx_pwrkey_platform_data *pdata;
+};
+
+static bool call_in_progress = false;
+static bool ischarging = false;
+static int sensor_wake_options = 0;
+static int sensor_wake_options_debug = 0;
+static bool wakelock_held = false;
+static struct wake_lock wakelock;
+//struct wakeup_source *ws;
+static DEFINE_MUTEX(prx_lock);
+static struct pmic8xxx_pwrkey *sensorwake_pwrdev;
+static struct delayed_work check_prox_val;
+static struct delayed_work sensorwake_presspwr_work;
+static bool trig_timer = false;
+static unsigned char prox_val = 0;
+static int prox_val_step_wave = 0;
+static unsigned long  prox_val_step_wave_tout = 0;
+static int prox_val_step_pocket = 0;
+static unsigned long  prox_val_step_pocket_tout = 0;
+static int prox_timer_length = 1000;
+struct device *gdev;
+#define PROX_CLEAR		25
+#define PROX_BLOCKED		220
+#define PROX_POCKET_LENGTH	10
 
 static ssize_t prox_vendor_show(struct device *dev,
 	struct device_attribute *attr, char *buf)
@@ -93,7 +132,7 @@ static ssize_t proximity_avg_store(struct device *dev,
 	return size;
 }
 
-static unsigned char get_proximity_rawdata(struct ssp_data *data)
+unsigned char get_proximity_rawdata(struct ssp_data *data)
 {
 	unsigned char uRowdata = 0;
 	char chTempbuf[2] = { 1, 20};
@@ -399,6 +438,44 @@ static ssize_t proximity_thresh_low_store(struct device *dev,
 	return size;
 }
 
+static ssize_t sensor_wake_options_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	int ret = sprintf(buf, "%d\n", sensor_wake_options);
+	return ret;
+}
+
+static ssize_t sensor_wake_options_store(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t size)
+{
+	int retval;
+	unsigned int val = 0;
+	gdev = dev;
+	retval = sscanf(buf, "%d", &val);
+	if (retval != 0 && val >= 0 && val <= 6)
+		sensor_wake_options = val;
+	return size;
+}
+
+static ssize_t sensor_wake_options_debug_show(struct device *dev,
+	struct device_attribute *attr, char *buf)
+{
+	int ret = sprintf(buf, "%d\n", sensor_wake_options_debug);
+	return ret;
+}
+
+static ssize_t sensor_wake_options_debug_store(struct device *dev,
+	struct device_attribute *attr, const char *buf, size_t size)
+{
+	int retval;
+	unsigned int val = 0;
+
+	retval = sscanf(buf, "%d", &val);
+	if (retval != 0 && val >= 0 && val <= 1)
+		sensor_wake_options_debug = val;
+	return size;
+}
+
 static ssize_t barcode_emul_enable_show(struct device *dev,
 	struct device_attribute *attr, char *buf)
 {
@@ -440,6 +517,10 @@ static DEVICE_ATTR(thresh_high, S_IRUGO | S_IWUSR | S_IWGRP,
 	proximity_thresh_high_show, proximity_thresh_high_store);
 static DEVICE_ATTR(thresh_low, S_IRUGO | S_IWUSR | S_IWGRP,
 	proximity_thresh_low_show, proximity_thresh_low_store);
+static DEVICE_ATTR(sensor_wake_options, S_IRUGO | S_IWUSR | S_IWGRP,
+	sensor_wake_options_show, sensor_wake_options_store);
+static DEVICE_ATTR(sensor_wake_options_debug, S_IRUGO | S_IWUSR | S_IWGRP,
+	sensor_wake_options_debug_show, sensor_wake_options_debug_store);
 
 static struct device_attribute *prox_attrs[] = {
 	&dev_attr_vendor,
@@ -451,13 +532,185 @@ static struct device_attribute *prox_attrs[] = {
 	&dev_attr_thresh_high,
 	&dev_attr_thresh_low,
 	&dev_attr_barcode_emul_en,
+	&dev_attr_sensor_wake_options,
+	&dev_attr_sensor_wake_options_debug,
 	NULL,
 };
+
+void set_call_in_progress_prox(bool state)
+{
+	call_in_progress = state;
+	if (sensor_wake_options_debug) pr_alert("CALL IN PROGRESS: %d\n", state);
+}
+
+void check_prox_value_trig(bool trig)
+{
+	if (sensor_wake_options && trig && !call_in_progress)
+	{
+		if (!main_prox_data->bProximityRawEnabled)
+		{
+			char chTempbuf[2] = { 1, 20};
+			send_instruction(main_prox_data, ADD_SENSOR, PROXIMITY_RAW, chTempbuf, 2);
+			main_prox_data->bProximityRawEnabled = true;
+		}
+		schedule_delayed_work_on(0, &check_prox_val, msecs_to_jiffies(prox_timer_length));
+	}
+	else
+	{
+		if (main_prox_data->bProximityRawEnabled)
+		{
+			char chTempbuf[2] = { 1, 20};
+			send_instruction(main_prox_data, REMOVE_SENSOR, PROXIMITY_RAW, chTempbuf, 2);
+			main_prox_data->bProximityRawEnabled = false;
+		}
+		prox_val_step_wave = 0;
+		prox_val_step_wave_tout = 0;
+		prox_val_step_pocket = 0;
+		prox_val_step_pocket_tout = 0;
+	}
+	trig_timer = trig;
+}
+
+void sensorwake_setdev(struct pmic8xxx_pwrkey *input_device) {
+	sensorwake_pwrdev = input_device;
+	return;
+}
+
+static void sensorwake_presspwr(struct work_struct *work) //(struct work_struct *screenwake_presspwr_work)
+{
+	if (sensor_wake_options_debug) pr_alert("SENSOR 2 WAKE - POWER BUTTON FUNC");
+	/*input_event(sensorwake_pwrdev, EV_KEY, KEY_POWER, 1);
+	msleep(100);
+	input_event(sensorwake_pwrdev, EV_SYN, 0, 0);
+	msleep(500);
+	input_event(sensorwake_pwrdev, EV_KEY, KEY_POWER, 0);
+	msleep(100);
+	input_event(sensorwake_pwrdev, EV_SYN, 0, 0);
+	msleep(500);*/
+	//wake_lock_timeout(&wakelock, msecs_to_jiffies(10000));
+	//pm_wakeup_event(gdev, 2000);
+	input_report_key(sensorwake_pwrdev->pwr, KEY_POWER, 1);
+	input_sync(sensorwake_pwrdev->pwr);
+	msleep(500);
+	input_report_key(sensorwake_pwrdev->pwr, KEY_POWER, 0);
+	input_sync(sensorwake_pwrdev->pwr);
+	//wake_unlock(&wakelock);
+	//mutex_unlock(&prx_lock);
+}
+//static DECLARE_WORK(sensorwake_presspwr_work, sensorwake_presspwr);
+
+void pwr_trig_fsensor(void)
+{
+	if (!call_in_progress)
+	{
+		//wake_lock_timeout(&wakelock, msecs_to_jiffies(10000));
+		//__pm_stay_awake(ws);
+		prox_val_step_wave = 0;
+		prox_val_step_wave_tout = 0;
+		prox_val_step_pocket = 0;
+		prox_val_step_pocket_tout = 0;
+		//if (mutex_trylock(&prx_lock)) 
+		//	schedule_work(&sensorwake_presspwr_work);
+		//wake_lock(&wakelock);
+		//msleep(100);
+		schedule_delayed_work_on(0, &sensorwake_presspwr_work, msecs_to_jiffies(200));
+	}
+}
+
+void ischarging_relay(bool status)
+{
+	ischarging = status;
+}
+
+static void check_prox_value(struct work_struct *work)
+{
+	unsigned char prox = main_prox_data->buf[PROXIMITY_RAW].prox[0];
+	
+	//Wave 2 wake
+	if (sensor_wake_options == 1 || (sensor_wake_options == 2 && ischarging) || sensor_wake_options == 5 || (sensor_wake_options == 6 && ischarging))
+	{
+		if (prox_val_step_wave == 0)
+		{
+			if (prox_val <= PROX_CLEAR && prox > prox_val+30)
+			{
+				prox_val_step_wave = 1;
+				if (!prox_val_step_wave_tout)
+					prox_val_step_wave_tout = jiffies_to_msecs(jiffies);
+				prox_timer_length = 50;
+			}
+		}
+		else if (prox_val_step_wave == 1 || prox_val_step_wave == 3)
+		{
+			if (prox > PROX_BLOCKED)
+				prox_val_step_wave++;
+		}
+		else if (prox_val_step_wave == 2 || prox_val_step_wave == 4)
+		{
+			if (prox <= PROX_CLEAR)
+			{
+				if (prox_val_step_wave == 2)
+					prox_val_step_wave = 3;
+				if (prox_val_step_wave == 4)
+				{
+					if (sensor_wake_options_debug) pr_alert("WAVE 2 WAKE - POWER BUTTON CALLING");
+					pwr_trig_fsensor();
+				}
+			}
+		}
+
+		prox_val = prox;
+		if (jiffies_to_msecs(jiffies) - prox_val_step_wave_tout > 4000)
+		{
+			prox_val_step_wave_tout = 0;
+			prox_val_step_wave = 0;
+			prox_timer_length = 1000;
+		}
+	}
+	
+	//Pocket 2 wake
+	if (sensor_wake_options == 3 || (sensor_wake_options == 4 && ischarging) || sensor_wake_options == 5 || (sensor_wake_options == 6 && ischarging))
+	{
+		if (prox_val_step_pocket == 0)
+		{
+			if (prox > PROX_BLOCKED)
+			{
+				prox_val_step_pocket = 1;
+				//if (!prox_val_step_pocket_tout)
+				prox_val_step_pocket_tout = jiffies_to_msecs(jiffies);
+			}
+		}
+		else if (prox_val_step_pocket == 1)
+		{
+			if (prox <= PROX_CLEAR)
+			{
+				if (jiffies_to_msecs(jiffies) - prox_val_step_pocket_tout >= 10000)
+				{
+					if (sensor_wake_options_debug) pr_alert("POCKET 2 WAKE - POWER BUTTON CALLING");
+					pwr_trig_fsensor();
+				}
+				else
+				{
+					prox_val_step_pocket = 0;
+					prox_val_step_pocket_tout = 0;
+				}
+			}
+		}
+	}
+	if (sensor_wake_options_debug) pr_alert("SENSOR 2 WAKE - Prox:%d ProxPrevious:%d W2W:%d P2W:%d W2WTimeout:%ld P2WTimeout:%ld\n", prox, prox_val, prox_val_step_wave, prox_val_step_pocket, jiffies_to_msecs(jiffies) - prox_val_step_wave_tout, jiffies_to_msecs(jiffies) - prox_val_step_pocket_tout);
+
+	if (trig_timer && !call_in_progress)
+		schedule_delayed_work_on(0, &check_prox_val, msecs_to_jiffies(prox_timer_length));
+}
 
 void initialize_prox_factorytest(struct ssp_data *data)
 {
 	sensors_register(data->prox_device, data,
 		prox_attrs, "proximity_sensor");
+	main_prox_data = data;
+	wake_lock_init(&wakelock, WAKE_LOCK_SUSPEND, "ktsensor_wake");
+	INIT_DELAYED_WORK(&check_prox_val, check_prox_value);
+	INIT_DELAYED_WORK(&sensorwake_presspwr_work, sensorwake_presspwr);
+	//ws = wakeup_source_register("kt_wakeup_func");		
 }
 
 void remove_prox_factorytest(struct ssp_data *data)
