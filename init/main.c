99c1ef5cc2780f82566472ca9dccc2c4c85501f9
ktoonsez <ktoonsez@gmail.com>
2014-03-25 20:16:14 -0700

    Fenny Fatals fix for reboot while trying to charge with phone powered off
    Conflicts:
    	init/main.c

--- init/main.c
+++ init/main.c
@@ -68,6 +68,7 @@
 #include <linux/shmem_fs.h>
 #include <linux/slab.h>
 #include <linux/perf_event.h>
+#include <s_funcs.h>
 
 #include <asm/io.h>
 #include <asm/bugs.h>
@@ -528,6 +529,7 @@ asmlinkage void __init start_kernel(void)
 	boot_init_stack_canary();
 	mm_init_owner(&init_mm, &init_task);
 	mm_init_cpumask(&init_mm);
+        replace_str((char*)&boot_command_line,"androidboot.bootchg=true","androidboot.mode=charger");
 	setup_command_line(command_line);
 	setup_nr_cpu_ids();
 	setup_per_cpu_areas();
