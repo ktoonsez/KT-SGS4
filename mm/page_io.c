0420a972d6944e2bf00c3e8ddf860196011d9046
ktoonsez <ktoonsez@gmail.com>
2014-03-26 08:21:02 -0700

    frontswap: Update page_io to include frontswap functions

--- mm/page_io.c
+++ mm/page_io.c
@@ -18,6 +18,9 @@
 #include <linux/bio.h>
 #include <linux/swapops.h>
 #include <linux/writeback.h>
+#ifdef CONFIG_FRONTSWAP
+#include <linux/frontswap.h>
+#endif
 #include <linux/aio.h>
 #include <linux/blkdev.h>
 #include <asm/pgtable.h>
@@ -140,6 +143,14 @@ int swap_writepage(struct page *page, struct writeback_control *wbc)
 		unlock_page(page);
 		goto out;
 	}
+#ifdef CONFIG_FRONTSWAP
+    if (frontswap_store(page) == 0) {
+            set_page_writeback(page);
+            unlock_page(page);
+            end_page_writeback(page);
+            goto out;
+    }
+#endif
 	bio = get_swap_bio(GFP_NOIO, page, end_swap_bio_write);
 	if (bio == NULL) {
 		set_page_dirty(page);
@@ -164,6 +175,13 @@ int swap_readpage(struct page *page)
 
 	VM_BUG_ON(!PageLocked(page));
 	VM_BUG_ON(PageUptodate(page));
+#ifdef CONFIG_FRONTSWAP
+    if (frontswap_load(page) == 0) {
+            SetPageUptodate(page);
+            unlock_page(page);
+            goto out;
+    }
+#endif
 	bio = get_swap_bio(GFP_KERNEL, page, end_swap_bio_read);
 	if (bio == NULL) {
 		unlock_page(page);
