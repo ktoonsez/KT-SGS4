584b23d88df344763d505f6968e5a9e5a8145062
ktoonsez <ktoonsez@gmail.com>
2014-03-26 19:57:08 -0700

    Bring back in synaptics_rmi4_f51_set_custom_rezero and synaptics_rmi4_reset_device functions to make sure screen is initialized

--- drivers/input/touchscreen/synaptics_i2c_rmi.c
+++ drivers/input/touchscreen/synaptics_i2c_rmi.c
@@ -1043,6 +1043,14 @@ static void check_options_while_soff(struct device *dev)
 		}
 
 		enable_irq_wake(rmi4_data->i2c_client->irq);
+		
+		retval = synaptics_rmi4_reset_device(rmi4_data);
+		if (retval < 0) {
+			dev_err(dev,
+					"%s: Failed to issue reset command, error = %d\n",
+					__func__, retval);
+		}
+		
 		screen_wake_options_when_off = screen_wake_options;
 	}
 }
@@ -3830,7 +3838,7 @@ static int synaptics_rmi4_reinit_device(struct synaptics_rmi4_data *rmi4_data)
 			__func__, f51->proximity_controls);
 
 #ifdef USE_CUSTOM_REZERO
-	//synaptics_rmi4_f51_set_custom_rezero(rmi4_data);
+	synaptics_rmi4_f51_set_custom_rezero(rmi4_data);
 
 	msleep(100);
 #endif
@@ -4738,6 +4746,11 @@ void set_screen_synaptic_off(void)
 			main_prox_data->bProximityRawEnabled = true;
 		}
 
+		retval = synaptics_rmi4_reset_device(rmi4_data);
+		if (retval < 0) {
+			pr_alert("%s: Failed to issue reset command, error = %d\n",
+					__func__, retval);
+		}
 		enable_irq_wake(rmi4_data->i2c_client->irq);
 		screen_wake_options_when_off = screen_wake_options;
 	}
@@ -4826,7 +4839,8 @@ void set_screen_synaptic_on(void)
 	if (!rmi4_data->irq_enabled)
 	{
 		enable_irq(rmi4_data->i2c_client->irq);
-		pr_alert("IRQ DISABLED FROM EXTERNAL");
+		if (screen_wake_options_when_off)
+			pr_alert("IRQ DISABLED FROM EXTERNAL");
 	}
 	rmi4_data->irq_enabled = true;
 	
