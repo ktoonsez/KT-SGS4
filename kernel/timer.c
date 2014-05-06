c5c07b2e5ba64c3523dedf423129918bde0cd947
ktoonsez <ktoonsez@gmail.com>
2014-05-05 19:48:54 -0700

    FINALLY FIXED MY LOCAL GIT.  And some tweaks from around the net.

--- kernel/timer.c
+++ kernel/timer.c
@@ -817,9 +817,7 @@ unsigned long apply_slack(struct timer_list *timer, unsigned long expires)
 
 	bit = __fls(mask);
 
-	mask = (1 << bit) - 1;
-
-	expires_limit = expires_limit & ~(mask);
+	expires_limit = (expires_limit >> bit) << bit;
 
 	return expires_limit;
 }
