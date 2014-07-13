6b709e73abe2d899ff998e318c3e9d92f4d044cf
ktoonsez <ktoonsez@gmail.com>
2014-07-12 21:11:00 -0700

    Remove 4.3 from build all branches

--- build_all_branches.sh
+++ build_all_branches.sh
@@ -29,67 +29,8 @@ echo "Make Mrproper"
 make mrproper
 ./build_intl.sh
 
-echo "Switch to AOSP 4.3"
-git checkout aosp4.3
-
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_att.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_spr.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_tmo.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_vzw.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_intl.sh
-
 echo "Switch to Touchjizz 4.4"
 git checkout tw4.4
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_att.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_spr.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_tmo.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_vzw.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_intl.sh
-
-
-echo "Switch to Touchjizz 4.3"
-git checkout tw4.3
-
 echo "Make Clean"
 make clean
 echo "Make Mrproper"
@@ -134,35 +75,6 @@ make clean
 echo "Make Mrproper"
 make mrproper
 ./build_vzw.sh
-
-echo "Switch to Touchjizz GE 4.3"
-git checkout twge4.3
-
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_att.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_spr.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_tmo.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_vzw.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_intl.sh
 
 echo "Switch to AOSP 4.4"
 git checkout aosp4.4
